c908cfc0ed1d2331fd8967d87f64b75b87a73589
Bremen Braun <bremen.braun@gmail.com>
2020-06-29 10:38:35 -0500

    feat: add associated drivers route

--- src/routes/drivers/__tests__/getAssociatedDrivers.test.js
+++ src/routes/drivers/__tests__/getAssociatedDrivers.test.js
@@ -1,3 +1,56 @@
-it('should work on second try', () => {
+import {iseCompliance} from '../../../services'
+import route from '../getAssociatedDrivers'
 
+jest.mock('../../../services')
+
+const setup = id => {
+    const request = {
+        headers: {
+            'x-application-customer': '00-0000-00'
+        },
+        params: {
+            id
+        },
+        auth: {
+            artifacts: {
+                user: {
+                    applicationCustomerUsers: [0]
+                }
+            }
+        }
+    }
+    const hapi = {
+        response: jest.fn(() => hapi),
+        code: jest.fn(() => hapi)
+    }
+
+    iseCompliance.get.mockClear()
+    return {request, hapi}
+}
+
+it('should get drivers associated with a vehicle', async () => {
+    const {request} = setup(1)
+    const {headers} = request
+
+    const expected = [{name: 'Speed Racer'}, {name: 'Racer X'}]
+
+    iseCompliance.get.mockResolvedValue(expected)
+    const drivers = await route.handler(request)
+
+    expect(drivers).toEqual(expected)
+    expect(iseCompliance.get).toHaveBeenCalledWith(`/api/vehicles/byVehicleId/1/drivers`, {headers})
 })
+
+it('should return an empty array if ISE returns a 404', async () => {
+    const {request} = setup(1)
+
+    iseCompliance.get.mockRejectedValue({
+        description: {
+            status: 404
+        }
+    })
+    const drivers = await route.handler(request)
+
+    expect(drivers).toEqual([])
+})
+
---
+++ src/services/iseCompliance.js
@@ -0,0 +1,45 @@
+import makeService from '@peoplenet/cf-services'
+import {logger} from '@peoplenet/node-service-common'
+
+const {iseCompliance: rawIseCompliance, connectedfleetcache} = makeService([
+    'iseCompliance',
+    'connectedfleetcache'
+])
+
+const getPfmIdForCustomer = async applicationCustomerId => {
+    try {
+        const {customer} = await connectedfleetcache.get(`/applicationCustomers/${applicationCustomerId}`, {headers: {'x-application-customer': applicationCustomerId}})
+        return customer.companyId
+    } catch (error) {
+        logger.error(error, 'Error Translating ED Customer ID to PFM Company ID')
+        throw error
+    }
+}
+
+const iseCompliance = (() => {
+    const cache = {}
+
+    return new Proxy(rawIseCompliance, {
+        get(target, prop) {
+            if (!['get', 'put', 'post', 'patch', 'delete'].includes(prop)) return target[prop]
+
+            return async (...args) => {
+                const {headers} = args.pop()
+                if (!cache.headers) {
+                    const pfmOrgId = await getPfmIdForCustomer(headers['x-application-customer'])
+                    cache.headers = {    
+                        'content-type': 'application/json',
+                        authorization: `Basic ${process.env.ISE_COMPLIANCE_AUTH}`,
+                        'x-authenticate-orgid': 'root',
+                        'x-filter-orgid': pfmOrgId
+                    }
+                }
+                
+                return rawIseCompliance[prop].apply(rawIseCompliance, [...args, {headers: cache.headers}])
+            }  
+        }
+    })
+})()
+
+export default iseCompliance
+

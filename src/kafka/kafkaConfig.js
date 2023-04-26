ebbffdf527e086c817bb3706335222ca0e897132
Andy Christianson <nix@nixxed.com>
2023-04-26 14:13:07 -0500

    fix: Add HOS topic to list.

--- src/kafka/kafkaConfig.js
+++ src/kafka/kafkaConfig.js
@@ -4,7 +4,8 @@ const {
     KAFKA_USER,
     KAFKA_PASS,
     KAFKA_RETRY_DELAY_MS = '10000',
-    KAFKA_AUTO_COMMIT_INTERVAL = '5000'
+    KAFKA_AUTO_COMMIT_INTERVAL = '5000',
+    ENV_PREFIX
 } = process.env
 
 // See consumption options & defaults here: https://kafka.js.org/docs/consuming#options
@@ -24,25 +25,28 @@ module.exports = {
     autoCommitInterval: +KAFKA_AUTO_COMMIT_INTERVAL,
     consumers: [
         {
-            topic: `aws.transportation.cmd.unique-group.2`,
+            topic: 'aws.transportation.cmd.unique-group.2',
             handlerPath: '/kafka/driverSettingsTemplate',
             handlerMethod: 'POST',
             useSchemaRegistry: true
         }, {
-            topic: `aws.transportation.cmd.managed-driver.0`,
+            topic: 'aws.transportation.cmd.managed-driver.0',
             handlerPath: '/kafka/driver',
             handlerMethod: 'POST',
             useSchemaRegistry: true
         }, {
-            topic: `aws.edw.fct.terminal.0`,
+            topic: 'aws.edw.fct.terminal.0',
             handlerPath: '/kafka/pfmTerminal',
-            handlerMethod: 'POST',
-            useSchemaRegistry: false
+            handlerMethod: 'POST'
         }, {
-            topic: `aws.edw.fct.driver.0`,
+            topic: 'aws.edw.fct.driver.0',
             handlerPath: '/kafka/pfmDriver',
-            handlerMethod: 'POST',
-            useSchemaRegistry: false
+            handlerMethod: 'POST'
+        }, {
+            enabled: ENV_PREFIX === 'staging' || ENV_PREFIX === 'prod',
+            topic: `${ENV_PREFIX}.trimble.transportation.edw.compliance.hours-of-service.qry`,
+            handlerPath: '/kafka/driverHoursOfService',
+            handlerMethod: 'POST'
         }
     ],
     producers: []

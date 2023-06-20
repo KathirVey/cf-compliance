a643540d16b75e4159c47321412c4b58076d7899
Smilesh P R <SRamesh1@eu.trimblecorp.net>
2023-06-20 15:36:44 +0530

    feat(driverLogs): EFSS-43618 New route for adding annotations

---
+++ src/routes/driverLogs/__tests__/addAnnotation.test.js
@@ -0,0 +1,39 @@
+import {compliance} from '../../../services'
+import route from '../addAnnotation'
+
+jest.mock('../../../services')
+process.env.ISE_COMPLIANCE_AUTH = 'someAuthToken'
+
+let request, iseHeaders
+
+beforeEach(() => {
+    request = {
+        auth: {
+            artifacts: {
+                hasPermission: jest.fn(),
+                user: {
+                    companyId: 'pfmCid',
+                    applicationCustomerId: 'user_ac_id'
+                }
+            }
+        },
+        headers: {
+            'x-application-customer': 'user_ac_id'
+        },
+        params: {
+            eventKey: 1
+        },
+        payload: 'test'
+    }
+    iseHeaders = {
+        'content-type': 'application/json',
+        authorization: `Basic someAuthToken`,
+        'x-authenticate-orgid': 'root',
+        'x-filter-orgid': 'pfmCid'
+    }    
+})
+
+it('should post annotation for the log event', async () => {    
+    await route.handler(request)
+    expect(compliance.post).toHaveBeenCalledWith(`/proxy/logEvents/addAnnotation/1`, 'test', {headers: iseHeaders})    
+})

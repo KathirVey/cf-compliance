6e6214b94821dd7a924909b656460c6f9ea0d14a
Prashant Shrestha <pshrestha@peoplenetonline.com>
2023-07-27 10:47:51 -0400

    fix: change log level to info on driver kafka endpoint

--- src/routes/kafka/driver.js
+++ src/routes/kafka/driver.js
@@ -27,7 +27,7 @@ module.exports = {
         } else if (method === 'DELETE') {
             await searchApi.delete('driver', entity)
         }
-        logger.debug({id: entity.id}, `Processed Driver ${method} event`)
+        logger.info({id: entity.id}, `Processed Driver ${method} event`)
         return hapi.response()
     },
     options: {
